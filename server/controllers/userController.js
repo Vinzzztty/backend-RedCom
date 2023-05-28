9f2f4096b228c95532b15d26982a92895a5bffde
Vinzzztty <kevinarnandes21@gmail.com>
2023-05-28 10:23:14 +0700

    Add GET All Users and User by ID, And Delete User by Id

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -15,3 +15,66 @@ exports.createUser = async (req, res) => {
         });
     }
 };
+
+/* 
+    GET ALL Users
+*/
+
+exports.getAllUser = async (req, res) => {
+    try {
+        const user = await User.find({}, "username email");
+
+        res.status(200).json({
+            data: user,
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: "Gagal Mendapatkan Semua User",
+        });
+    }
+};
+
+/*
+    GET Specific User by Id
+*/
+
+exports.getSpecificUser = async (req, res) => {
+    try {
+        const userId = req.params.id;
+
+        // Find Specific User by Id
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({
+                error: "User not found",
+            });
+        }
+
+        res.status(200).json({
+            data: user,
+            message: "Successfully GET the User Id",
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: "Gagal Mendapatkan User dengan ID itu",
+        });
+    }
+};
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const userId = req.params.id;
+
+        // Delete the user from the database
+        await User.findByIdAndDelete(userId);
+
+        res.status(200).json({
+            message: "User deleted successfully",
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: "Error while delete user",
+        });
+    }
+};
