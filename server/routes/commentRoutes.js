f8c845232f57f4b1c40aeaf37a9d2e78764606c6
Vinzzztty <kevinarnandes21@gmail.com>
2023-05-30 10:19:10 +0700

    Complete CRUD Routes API for Comment

--- server/routes/commentRoutes.js
+++ server/routes/commentRoutes.js
@@ -2,7 +2,19 @@ const express = require("express");
 const router = express.Router();
 const commentController = require("../controllers/commentController");
 
-// Create a new post
+// POST Create a new comments
 router.post("/comments", commentController.createComment);
+
+// GET Show All Comments
+router.get("/comments", commentController.showAllComments);
+
+// GET Show Specific Comment by Id
+router.get("/comments/:id", commentController.getCommentById);
+
+// PUT Edit Comment
+router.put("/comments/edit/:id", commentController.editComment);
+
+// Delete
+router.delete("/comments/:id", commentController.deleteComment);
 
 module.exports = router;
