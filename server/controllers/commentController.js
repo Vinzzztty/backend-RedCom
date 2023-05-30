c540ca9952c73bb8bc594df72bbebe00dc6a9539
Vinzzztty <kevinarnandes21@gmail.com>
2023-05-30 10:19:57 +0700

    Complete CRUD Method API for Comment

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -16,3 +16,89 @@ exports.createComment = async (req, res) => {
         });
     }
 };
+
+/**
+ * GET
+ * All Comments
+ */
+
+exports.showAllComments = async (req, res) => {
+    try {
+        const comments = await Comment.find();
+
+        res.status(200).json({
+            data: comments,
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: "Error ketika mendapatkan data Comments",
+        });
+    }
+};
+
+/**
+ * GET
+ * Specific Comment by Id
+ */
+
+exports.getCommentById = async (req, res) => {
+    try {
+        const commentId = req.params.id;
+
+        // Find Specific Comment by Id
+        const comment = await Comment.findById(commentId);
+
+        res.status(200).json({
+            data: comment,
+            message: "Successfully GET the Comment Id",
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: "Error ketika mendapatkan data comment",
+        });
+    }
+};
+
+/**
+ * PUT
+ * Edit Specific Comment
+ */
+exports.editComment = async (req, res) => {
+    try {
+        const commentId = req.params.id;
+        const { text } = req.body;
+
+        // Find the comment by Id and update the fields
+        const updatedComment = await Comment.findByIdAndUpdate(
+            commentId,
+            { text },
+            { new: true }
+        );
+
+        res.status(200).json({
+            message: "Comment Update Successfully",
+            data: updatedComment,
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: "Error ketika mendapatkan data Comment",
+        });
+    }
+};
+
+exports.deleteComment = async (req, res) => {
+    try {
+        const commentId = req.params.id;
+
+        // Delete the comment from the database
+        await Comment.findByIdAndDelete(commentId);
+
+        res.status(200).json({
+            message: "Comment deleted successfully",
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: "Error ketika deleting comment",
+        });
+    }
+};
