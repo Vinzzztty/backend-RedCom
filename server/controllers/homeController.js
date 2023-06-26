f6451cd4be7f3f807d43de9868b35e25031a9269
Vinzzztty <kevinarnandes21@gmail.com>
2023-06-26 09:42:03 +0700

    feat: create new search feature

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,14 +1,5 @@
 const Posts = require("../models/Post");
 
-// module.exports = {
-//     home: async (req, res) => {
-//         try {
-//             const post = await Posts.find().populate("user");
-//             res.render("home", { post });
-//             console.log(post);
-//         } catch (err) {}
-//     },
-// };
 exports.home = async (req, res) => {
     try {
         const post = await Posts.find().populate("user_id");
@@ -27,6 +18,30 @@ exports.tes = async (req, res) => {
         res.status(200).json({
             message: "Masuk",
         });
+    } catch (error) {
+        res.status(500).json({
+            error: error,
+            message: "An unexpected error occurred",
+        });
+    }
+};
+
+/**
+ * Search Post Data
+ */
+exports.search = async (req, res) => {
+    try {
+        let searchPost = req.body.searchPost;
+        const searchNoSpecialChar = searchPost.replace(/[^a-zA-Z0-9 ]/g, "");
+
+        const post = await Posts.find({
+            $or: [
+                { content: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+            ],
+        }).populate("user_id");
+
+        res.render("search", { post });
+        return;
     } catch (error) {
         res.status(500).json({
             error: error,
--- server/routes/homeRoutes.js
+++ server/routes/homeRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/homeController");
 
-//router.get("/", homeController.home);
-
 router.get("/", homeController.home);
 
 router.get("/tes", homeController.tes);
+
+router.post("/search", homeController.search);
 
 module.exports = router;
