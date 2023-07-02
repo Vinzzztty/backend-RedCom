7cb9af77827cff1458fe5f5a0ccfa79879ed0dc9
Vinzzztty <kevinarnandes21@gmail.com>
2023-07-02 23:21:53 +0700

    fix: fix cors for api consume to frontend

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const connectDB = require("./server/config/dbConfig");
+const cors = require("cors");
 
 const { verifyAccessToken } = require("./server/middleware/jwt_helper");
 
@@ -16,13 +17,14 @@ const authRoutes = require("./server/routes/authRoute");
 const app = express();
 const port = 5000 || process.env.PORT;
 
-// Connect to MongoDB
-connectDB();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
+
+// Connect to MongoDB
+connectDB();
 
 // Routes
 app.use("/api/users", userRoutes);
