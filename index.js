1ec91e025f33d0750d72fa8abc9ad7d56beef584
Vinzzztty <kevinarnandes21@gmail.com>
2023-07-04 18:08:06 +0700

    fix: try

--- index.js
+++ index.js
@@ -16,13 +16,14 @@ const port = 5000 || process.env.PORT;
 
 // CORS configuration
 app.set("trust proxy", 1);
-app.use(
-    cors({
-        origin: "*",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true,
-    })
-);
+app.use(cors());
+// app.use(
+//     cors({
+//         origin: "*",
+//         methods: ["GET", "POST", "PUT", "DELETE"],
+//         credentials: true,
+//     })
+// );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
